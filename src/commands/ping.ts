6a45b42e0ae41e324d7078beba43cf8da8152ba7
nyani <99234942+pnyani@users.noreply.github.com>
2025-12-31 14:44:19 +0900

    feat: Display delay time in ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,5 +1,11 @@
 import { ChatInputCommandInteraction } from 'discord.js';
 
 export async function handlePing(interaction: ChatInputCommandInteraction): Promise<void> {
-  await interaction.reply('Pong!');
+  const sentTimestamp = Date.now();
+  await interaction. deferReply();
+  const latency = Date.now() - sentTimestamp;
+
+  await interaction.editReply(
+    `Pong!\nWebsocket Heartbeat: ${interaction.client.ws.ping}ms\nRoundtrip Latency: ${latency}ms`
+  );
 }
